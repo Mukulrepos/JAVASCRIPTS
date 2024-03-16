32585fbd91f8ef77120cf6440becb3ded4df14c9
Mukulrepos <mv282301@gmail.com>
2024-03-16 17:01:29 +0530

    Conversion with superfaster ways

---
+++ BASIC/Conversion.js
@@ -0,0 +1,35 @@
+let score = "33as"; // swapp with null data type it can print type a number but print the 0
+
+// console.log(typeof score); // check any variable value using the typeof
+
+// let perform_number= Number(score)
+// console.log(typeof(perform_number));
+// console.log(perform_number);
+
+// if any body number with some string it can print the NaN 
+// type
+
+
+// change the string into number
+// using the  data_type_variable_name(change variable name)
+
+
+
+let login = 1
+let solove = Boolean(login)
+console.log(solove);
+console.log(typeof(solove));
+
+
+
+let tv_ka_channel_number = 34
+let intype= String(tv_ka_channel_number)// it can print" 34"
+console.log(intype);
+console.log(typeof(intype));
+
+
+
+let names = 1
+let typrs =- Boolean(names)
+console.log(typrs);
+console.log(typeof(typrs));
