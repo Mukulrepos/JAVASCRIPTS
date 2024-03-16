427833d23a2ef34ef6a55820c465ff0523245126
Mukulrepos <mv282301@gmail.com>
2024-03-16 16:46:38 +0530

    hello

---
+++ BASIC/ECMA.js
@@ -0,0 +1,27 @@
+//  code readablity is good
+//  make some sense
+// make good logic using the DSA
+
+
+console.log("hi");
+
+let fname = "mukul"
+let age = 45
+let job = "Developer"
+
+
+
+// number ==> 34
+// bignit
+// string
+// boolean
+// symbol
+// null
+// undefined
+
+
+// non primitive is
+//  object 
+// function
+// array
+
