d6acb92b935c1f7dbf5ba85b5bb635e32d24a76b
Mukulrepos <mv282301@gmail.com>
2024-03-16 16:39:57 +0530

    hello

--- BASIC/HELLO.js
+++ BASIC/HELLO.js
@@ -1,1 +1,31 @@
 console.log("MUKUL VATS");
+
+const AccountID = 23456; // wasy to treadable
+let Account_email = "mukul@gmail";
+var Account_password = "123456789";
+accountliby = "maujpur";
+
+
+// key word is already define in the node js
+// const  == it can be change the function
+// let == it can be change but use  scoppe
+
+
+// AccountID = 23
+// console.log(AccountID);// it can give error in constant varibale cannot be change the valuue inside it
+
+
+// comment baba {//}
+
+Account_email = "jsks"
+console.log(Account_email);
+
+Account_password = "45"
+
+accountliby ="delhi"
+
+console.table([Account_email,Account_password,accountliby])
+
+
+let id;
+console.log(id);// mean the value is not input by user
