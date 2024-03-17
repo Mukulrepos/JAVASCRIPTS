2cdf088203bfb183c11a1443ff79427e3b27c6eb
Mukulrepos <mv282301@gmail.com>
2024-03-17 16:29:10 +0530

    Comparison

---
+++ BASIC/Comparison.js
@@ -0,0 +1,27 @@
+// comaprion solve a differnt type of problem like maths
+// age
+// price
+// standard
+// goals
+
+// ======================================================
+//                       JAVASCRIPT(COMAPRISONS)
+console.log(2>1);// give true
+console.log(2>=1);// true
+console.log(2<1);//false
+console.log(2<=4);//true
+
+ console.log("2">1);
+ console.log("02">1);
+ console.log("2">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
+// giveus false
