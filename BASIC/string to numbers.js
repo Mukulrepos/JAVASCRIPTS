0ccb15a579c184e5d7b7bb3779585d2e38223a30
Mukulrepos <mv282301@gmail.com>
2024-03-17 16:18:08 +0530

    string to number

---
+++ BASIC/string to numbers.js
@@ -0,0 +1,50 @@
+let some = 33;
+let chagnger = String(some);
+// console.log(typeof chagnger); // print the type int string
+// console.log(chagnger); // answer the type in number
+
+
+// string to number
+
+let fname = "gopal"
+let numbers = Number(fname)
+console.log(typeof(numbers));// type convrt into Number
+console.log(numbers);// but give the Not an NUmber
+
+// ===============================operations
+
+console.log(2+1);
+console.log(2-3);
+console.log(2*1);
+console.log(2**2);
+console.log(2/2);
+console.log(2%1);
+
+
+
+// lets connect two string in javascript using + sign s
+
+let f_name= "ram"
+let s_name = " yadav"
+
+let full_name= f_name + s_name
+
+console.log(full_name);
+console.log(typeof(full_name));
+
+
+console.log(234+66);
+console.log(Number("234")+66);// string + number
+console.log(234+Number("66")+1);// string + number + string 
+console.log(234+Number("66"));//number + string
+
+
+
+
+
+let num1, num2 , num3 
+
+num1= num2= num3 = 234
+let gamecounter=  100
+++gamecounter
+console.log(gamecounter);
