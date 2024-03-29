d6c0ff5a5b43d09c3cb26aa93c8b3d86a8d9de77
Mukulrepos <mv282301@gmail.com>
2024-03-29 15:18:44 +0530

    scopes

---
+++ BASIC/scopes.js
@@ -0,0 +1,51 @@
+// // // var c=200
+// // let a = 300;
+// // if (true) {
+// //   let a = 10;
+// //   const b = 90;
+// //   console.log(a);
+// //   //   var c = 10;
+// // }
+// // console.table([a]);
+
+// // //  scope level part 2
+
+// // function one() {
+// //   const customer = " vivek ";
+// //   function two() {
+// //     const robot = "live";
+// //     console.log(customer);
+// //   }
+
+// //   two();
+// // }
+// // // two()
+
+// // one();
+
+
+// if( true ){
+
+//     const customer = "mukul"
+//     if(customer === "mukul"){
+//         const website = " youtube"
+//         console.log(customer+website);
+//     }
+//     // console.log(website);
+// }
+// // console.log(customer);
+addone(99)
+
+// -----------------------------------------------------------------
+function  addone(num){
+
+
+    return num +1
+}
+
+addtwo(99)
+
+const addtwo=function(num){
+    return num +2
+}
+
